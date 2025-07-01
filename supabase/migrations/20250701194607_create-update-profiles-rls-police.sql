ef46466a419b0807c291d871ea5970493d1cfb28
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-01 16:47:25 -0300

    fix: adding update migration for profiles

---
+++ supabase/migrations/20250701194607_create-update-profiles-rls-police.sql
@@ -0,0 +1,11 @@
+DROP POLICY IF EXISTS "Enable update for users based on email" ON public.profiles;
+
+create policy "Enable update for users based on email"
+on public.profiles
+to public
+using (
+  ((( SELECT auth.jwt() AS jwt) ->> 'email'::text) = email)
+)
+with check (
+  ((( SELECT auth.jwt() AS jwt) ->> 'email'::text) = email)
+);
