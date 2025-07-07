bea098b49d26f2a55e7c8269ad2e3078d049f34e
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-07 09:47:01 -0300

    feat: criando migração para transferencia de dados entre asset_client_assoc e associations

---
+++ supabase/migrations/20250707120539_moving-old_associations-to-the-new-table.sql
@@ -0,0 +1,37 @@
+INSERT INTO associations (
+  uuid,
+  client_id,
+  equipment_id,
+  chip_id,
+  entry_date,
+  exit_date,
+  association_type_id,
+  plan_id,
+  plan_gb,
+  equipment_ssid,
+  equipment_pass,
+  status,
+  notes,
+  created_at,
+  updated_at,
+  deleted_at
+)
+SELECT
+  gen_random_uuid(),
+  aca.client_id,
+  CASE WHEN a.solution_id = 11 THEN NULL ELSE aca.asset_id END AS equipment_id,
+  CASE WHEN a.solution_id = 11 THEN aca.asset_id ELSE NULL END AS chip_id,
+  aca.entry_date,
+  aca.exit_date,
+  aca.association_id,
+  aca.plan_id,
+  aca.gb,
+  aca.ssid,
+  aca.pass,
+  (aca.exit_date IS NULL) AS status,
+  aca.notes,
+  aca.created_at,
+  aca.updated_at,
+  aca.deleted_at
+FROM asset_client_assoc aca
+JOIN assets a ON aca.asset_id = a.uuid;
