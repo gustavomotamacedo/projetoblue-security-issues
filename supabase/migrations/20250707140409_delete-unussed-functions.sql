df2bb21a788444fc5e74e7f4e364b11b428781aa
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-10 15:16:28 -0300

    Update 20250707140409_delete-unussed-functions.sql

--- supabase/migrations/20250707140409_delete-unussed-functions.sql
+++ supabase/migrations/20250707140409_delete-unussed-functions.sql
@@ -1,12 +1,3 @@
-DROP FUNCTION IF EXISTS admin_list_users CASCADE;
-DROP FUNCTION IF EXISTS admin_update_user_profile CASCADE;
-DROP FUNCTION IF EXISTS admin_update_user_role CASCADE;
-DROP FUNCTION IF EXISTS check_association_assets CASCADE;
-
--- Drop related functions
-DROP FUNCTION IF EXISTS public.prevent_rented_association() CASCADE;
-DROP FUNCTION IF EXISTS public.validate_active_uuid() CASCADE;
-
 -- Drop triggers on asset_client_assoc table
 DROP TRIGGER IF EXISTS asset_client_assoc_log_trigger ON public.asset_client_assoc;
 DROP TRIGGER IF EXISTS asset_client_assoc_status_log_trigger ON public.asset_client_assoc;
@@ -15,3 +6,12 @@ DROP TRIGGER IF EXISTS log_and_update_status_on_status_change ON public.asset_cl
 DROP TRIGGER IF EXISTS prevent_rented_association_before ON public.asset_client_assoc;
 DROP TRIGGER IF EXISTS set_asset_client_assoc_updated_at ON public.asset_client_assoc;
 DROP TRIGGER IF EXISTS validate_active_uuid_on_change ON public.asset_client_assoc;
+
+DROP FUNCTION IF EXISTS admin_list_users CASCADE;
+DROP FUNCTION IF EXISTS admin_update_user_profile CASCADE;
+DROP FUNCTION IF EXISTS admin_update_user_role CASCADE;
+DROP FUNCTION IF EXISTS check_association_assets CASCADE;
+
+-- Drop related functions
+DROP FUNCTION IF EXISTS public.prevent_rented_association() CASCADE;
+DROP FUNCTION IF EXISTS public.validate_active_uuid() CASCADE;
