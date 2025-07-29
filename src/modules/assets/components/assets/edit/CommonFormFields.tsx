0c143727a8cb00e9d52d46af56fb387f2768dbdb
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-29 15:01:47 -0300

    Update CommonFormFields.tsx

--- src/modules/assets/components/assets/edit/CommonFormFields.tsx
+++ src/modules/assets/components/assets/edit/CommonFormFields.tsx
@@ -1,18 +1,22 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Manufacturer } from '@/types/asset';
+import { AssetSolution, Manufacturer } from '@/types/asset';
+import { supabase } from '@/integrations/supabase/client';
+import { capitalize } from '@/utils/stringUtils';
 
 interface CommonFormFieldsProps {
   formData: {
     status_id?: number;
     manufacturer_id?: number;
+    solution_id: number;
   };
   isChip: boolean;
+  manufacturers: Manufacturer[];
   handleStatusChange: (value: string) => void;
   handleManufacturerChange: (value: string) => void;
-  manufacturers: Manufacturer[];
+  handleSolutionChange: (value: string) => void;
 }
 
 const CommonFormFields = ({ 
@@ -20,8 +24,35 @@ const CommonFormFields = ({
   isChip, 
   handleStatusChange, 
   handleManufacturerChange, 
+  handleSolutionChange,
   manufacturers 
 }: CommonFormFieldsProps) => {
+
+  const [solutions, setSolutions] = useState<AssetSolution[]>([]);
+
+  useEffect(() => {
+    const fetchSolutions = async () => {
+        const {data, error: solutionsError} = await supabase
+        .from('asset_solutions')
+        .select('*')
+        .order('solution');
+
+      if(solutionsError) throw new Error('Não foram encontradas as soluções da LEGAL.');
+
+      const mappedSolutions = data.map(s => ({
+        id: s.id, // bigint NOT NULL
+        solution: s.solution, // text NOT NULL
+        created_at: s.created_at, // timestamp NOT NULL
+        updated_at: s.updated_at, // timestamp NOT NULL
+        deleted_at: s.deleted_at  // timestamp nullable
+      }));
+
+      setSolutions(mappedSolutions);
+    }
+
+    fetchSolutions();
+  }, []);
+
   return (
     <>
       <div className="space-y-2">
@@ -40,6 +71,25 @@ const CommonFormFields = ({
             <SelectItem value="4">SEM DADOS</SelectItem>
             <SelectItem value="5">BLOQUEADO</SelectItem>
             <SelectItem value="6">MANUTENÇÃO</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      
+      <div className="space-y-2">
+        <Label htmlFor="solution_id">Solução</Label>
+        <Select
+          value={formData.solution_id?.toString()}
+          onValueChange={handleSolutionChange}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Selecione o tipo de solução" />
+          </SelectTrigger>
+          <SelectContent>
+            {solutions.map(solution => (
+              <SelectItem key={solution.id} value={solution.id.toString()}>
+                { solution.solution }
+              </SelectItem>
+            ))}            
           </SelectContent>
         </Select>
       </div>
